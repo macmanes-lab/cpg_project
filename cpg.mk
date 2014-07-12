2351be738568a06643db9f94d2c27356223a8d63
Matt MacManes <macmanes@gmail.com>
2014-07-12 12:27:23 -0400

    status messages

--- cpg.mk
+++ cpg.mk
@@ -25,10 +25,14 @@ AUGMENT=10
 RUN=run
 
 tmp:$(IN)
+	@echo '\n\n'BEGIN: `date +'%a %d%b%Y  %H:%M:%S'`
+	@echo Results will be in a file named $(RUN).clust
+	@echo The format of this file is 'Contig name   CpG Start Position   CpG Length   %GC   Obs/Exp'
 	python cpg.py -i $(IN) -a $(AUGMENT) -w $(WINDOW)| awk '$(OE)>$$4{next}1' | tee $(RUN).cpg | awk '{print $$2}' > tmp
 $(RUN).clust:tmp
 	python clust.py | sort -nk4 > tmp4 # good here
 	cat tmp4 | awk '{print $$4}' > tmp1
 	grep -wf tmp1 $(RUN).cpg > tmp2
 	paste tmp4 tmp2 | awk '{print $$5 "\t" $$3 $$4 "\t" $$2 "\t" $$7 "\t" $$8}' > $(RUN).clust
-	#rm tmp tmp2 tmp4 tmp1
+	rm tmp tmp2 tmp4 tmp1
+	@echo END: `date +'%a %d%b%Y  %H:%M:%S'`'\n\n'
